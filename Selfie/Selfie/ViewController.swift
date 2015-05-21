c0a1e3cac420c2575ab3981199ecb5769fce045c
xl <zjlsxulei@gmail.com>
2015-05-21 22:35:28 +0800

    新建Selfie

---
+++ Selfie/Selfie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfie
+//
+//  Created by 徐磊 on 15/5/21.
+//  Copyright (c) 2015年 徐磊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
