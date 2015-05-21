6ed19e06b02199e47dc3c8ca13b7f0217ebb644e
xl <zjlsxulei@gmail.com>
2015-05-21 23:32:52 +0800

    添加了分享照片到Facebook的功能~

--- Selfie/Selfie/ViewController.swift
+++ Selfie/Selfie/ViewController.swift
@@ -7,9 +7,40 @@
 //
 
 import UIKit
+import Social
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBAction func shareTapped(sender: AnyObject) {
+        var social = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+        social.addImage(myImageView.image)
+        
+        self.presentViewController(social, animated: true, completion: nil)
+    }
+    
+    @IBAction func selfieTapped(sender: AnyObject) {
+        var imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+            imagePicker.sourceType = .Camera
+            if(UIImagePickerController.isCameraDeviceAvailable(.Front)){
+                imagePicker.cameraDevice = .Front
+            }else{
+                imagePicker.cameraDevice = .Rear
+            }
+        }else{
+            imagePicker.sourceType = .PhotoLibrary
+        }
+        
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        myImageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBOutlet weak var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
